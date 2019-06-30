cb8bade7995fc01cb790600c240c3db287ebd345
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-07-01 00:10:14 +0100

    fixed and better priority queue

--- src/test/scala/queue/PriorityQueueSpec.scala
+++ src/test/scala/queue/PriorityQueueSpec.scala
@@ -3,66 +3,114 @@ package queue
 import org.scalatest.FunSuite
 import scala.collection.mutable.ArrayBuffer
 
-class Node(
-            val data: String = "",
-            val priority: Int = 0
-) {
-  private var next: Option[Node] = None
+case class Node(data: String, priority: Int)
 
-  def getNext(): Option[Node] = next
-  def setNext(nextNode: Option[Node]): Node = {
-    next = nextNode
-    this
-  }
+class PriorityQueue() {
+    private var q = ArrayBuffer[Node]()
 
-  def addNode(newNode: Node): Node = {
-    if(newNode.priority > this.priority){
-      newNode.setNext(Some(this))
-      newNode
-    } else {
-      this.getNext() match {
-        case Some(nextNode) => nextNode.addNode(newNode)
-        case None => this.setNext(Some(newNode))
+    def nqueue(newitem: Node) = {
+      var inserted = false
+      for((node, ix) <- q.zipWithIndex if inserted == false) {
+        if(newitem.priority > node.priority){
+          q.insert(ix, newitem)
+          inserted = true
+        }
       }
-      this
-    }
-  }
 
-  def traverse(journey: ArrayBuffer[String]): ArrayBuffer[String] = {
-    journey += this.data
-
-    this.getNext() match {
-      case Some(node) => node.traverse(journey)
-      case None => journey
+      if (inserted == false) {
+        q += newitem
+      }
     }
-  }
+
+    def dqueue(): Node = {
+      q.remove(0)
+    }
+
+    def getQ() = q
+
+    def show(): Unit = {
+      q.foreach(println(_))
+    }
 }
 
 class PriorityQueueSpec extends FunSuite {
-
-  test("I can traverse across multiple nodes") {
-    val nodeC = new Node("C", 0)
-    val nodeB = new Node("B", 2).setNext(Some(nodeC))
-    val nodeA = new Node("A", 100).setNext(Some(nodeB))
-
-    assert(nodeA.traverse(ArrayBuffer()) === ArrayBuffer("A", "B", "C"))
+  test("ArrayBuffer inser") {
+    val a = ArrayBuffer(1,3,7,9)
+    a.insert(2,100)
+    assert(a === ArrayBuffer(1, 3, 100, 7, 9))
   }
 
-  test("can place new node in proper place") {
-    val nodeD10 = new Node("D10", 10)
-    val nodeC3 = new Node("C3", 3)
-    val nodeB8 = new Node("B8", 8)
-    val nodeA5 = new Node("A5", 5)
-    val nodeF7 = new Node("F7", 7)
-    val nodeE6 = new Node("E6", 6)
+  test("I can get indexes in a for loop") {
+    val a = ArrayBuffer(1, 3, 100, 7, 9)
+    for((item, ix) <- a.zipWithIndex) {
+      println(ix + " => " + item)
+      //      0 => 1
+      //      1 => 3
+      //      2 => 100
+      //      3 => 7
+      //      4 => 9
+    }
+  }
 
-    val queue = nodeA5
-      .addNode(nodeB8)
-      .addNode(nodeC3)
-      .addNode(nodeD10)
-      .addNode(nodeF7)
-      .addNode(nodeE6)
+  test("I can insert most basic cases") {
+    val q = new PriorityQueue()
+    val nodeD10 = Node("D10", 10)
+    val nodeC3 = Node("C3", 3)
 
-    assert(queue.traverse(ArrayBuffer()) === ArrayBuffer("D10", "B8", "F7", "E6", "A5", "C3"))
+    q.nqueue(nodeD10)
+    q.nqueue(nodeC3)
+
+    assert(q.getQ() === ArrayBuffer(
+      nodeD10,
+      nodeC3)
+    )
+
+  }
+
+  test("I can set new nodes in the proper position") {
+    val q = new PriorityQueue()
+    val nodeD10 = Node("D10", 10)
+    val nodeC3 = Node("C3", 3)
+    val nodeB8 = Node("B8", 8)
+    val nodeA5 = Node("A5", 5)
+    val nodeF7 = Node("F7", 7)
+    val nodeE6 = Node("E6", 6)
+
+    q.nqueue(nodeA5)
+    q.nqueue(nodeB8)
+    q.nqueue(nodeC3)
+    q.nqueue(nodeD10)
+    q.nqueue(nodeF7)
+    q.nqueue(nodeE6)
+
+    assert(q.getQ() === ArrayBuffer(
+      nodeD10,
+      nodeB8,
+      nodeF7,
+      nodeE6,
+      nodeA5,
+      nodeC3)
+    )
+
+  }
+
+  test("Can dequeue the most priority") {
+    val q = new PriorityQueue()
+    val nodeD10 = Node("D10", 10)
+    val nodeC3 = Node("C3", 3)
+    val nodeB8 = Node("B8", 8)
+    val nodeA5 = Node("A5", 5)
+    val nodeF7 = Node("F7", 7)
+    val nodeE6 = Node("E6", 6)
+
+    q.nqueue(nodeA5)
+    q.nqueue(nodeB8)
+    q.nqueue(nodeC3)
+    q.nqueue(nodeD10)
+    q.nqueue(nodeF7)
+    q.nqueue(nodeE6)
+
+    assert(q.dqueue() == nodeD10)
+    assert(q.dqueue() == nodeB8)
   }
 }
