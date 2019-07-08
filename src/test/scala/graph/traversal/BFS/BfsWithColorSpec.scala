61640b745004a5a2791df9a446023837c6b506f1
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-07-08 23:32:21 +0100

    BSF using colors

--- src/test/scala/graph/traversal/BFS/BfsWithColorSpec.scala
+++ src/test/scala/graph/traversal/BFS/BfsWithColorSpec.scala
@@ -1,20 +1,46 @@
 package graph.traversal.BFS
 
 import org.scalatest.FunSuite
+import DataStructure.queue.Queue
+import scala.collection.immutable.ListMap
 
 class BfsWithColorSpec extends FunSuite {
 
+  def bsf_iterate(
+           Adj: ListMap[NodeColor, List[NodeColor]],
+           s: NodeColor
+      ): Unit = {
+    val queue = new Queue[NodeColor]()
+    s.color = "gray"
+    queue.nque(s)
+
+    while(!queue.isEmpty()) {
+      var u = queue.dque()  // all items in the queue are gray (discovered, ready to explore)
+
+      for(v <- Adj.get(u).get) {
+        if(v.color == "white") {
+          v.color = "gray"
+          queue.nque(v)
+        }
+      }
+
+      u.color = "black" // we have discovered all the inmediate-destinations of the current node, so it has been processed
+    }
+  }
+
+
   test("I can modify object inside of inmutable List") {
     val node1 = NodeColor("a value")
+    assert(node1.color === "white")
     val adjacencyList = List(
       node1
     )
 
     for(node <- adjacencyList) {
-      node.color = "modified"
+      node.color = "gray"
     }
 
-    assert(adjacencyList(0).color === "modified")
+    assert(adjacencyList(0).color === "gray")
   }
 
   test("I can iterate") {
@@ -24,22 +50,22 @@ class BfsWithColorSpec extends FunSuite {
     val nodeD = NodeColor("D")
     val nodeE = NodeColor("E")
     val nodeF = NodeColor("F")
-    val nodeG = NodeColor("F")
+    val nodeG = NodeColor("G")
     val nodeH = NodeColor("H")
     val nodeI = NodeColor("I")
 
-
-    val adjacencyList = List(
-      nodeA.conn(nodeB).conn(nodeC),
-      nodeB.conn(nodeA).conn(nodeF),
-      nodeC.conn(nodeA),
-      nodeD.conn(nodeA).conn(nodeG),
-      nodeE.conn(nodeA),
-      nodeF.conn(nodeB).conn(nodeH),
-      nodeG.conn(nodeD).conn(nodeI),
-      nodeH.conn(nodeF),
-      nodeI.conn(nodeG)
+    val adjacencyList = ListMap(
+      nodeA -> List(nodeB, nodeC),
+      nodeB -> List(nodeA, nodeF),
+      nodeC -> List(nodeA),
+      nodeD -> List(nodeA, nodeG),
+      nodeE -> List(nodeA),
+      nodeF -> List(nodeB,nodeH),
+      nodeG -> List(nodeD,nodeI),
+      nodeH -> List(nodeF),
+      nodeI -> List(nodeG)
     )
-  }
 
+    bsf_iterate(adjacencyList, nodeA)
+  }
 }
