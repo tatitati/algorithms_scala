bd411674ba400564304b6d5e00d91c7d976e57f5
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-07-10 00:58:15 +0100

    bellmanford done

--- src/test/scala/graph/shortestpath/BellmanFSpec.scala
+++ src/test/scala/graph/shortestpath/BellmanFSpec.scala
@@ -3,7 +3,6 @@ package graph.shortestpath
 import org.scalatest.FunSuite
 import DataStructure.queue.Queue
 import graph.traversal.BFS.NodeColor
-
 import scala.collection.SortedMap
 import scala.collection.mutable.ListMap
 import scala.collection.mutable.ArrayBuffer
@@ -16,6 +15,7 @@ case class Edge(
 )
 
 case class Vertex(
+   id: String,
    var distance: Double,
    edgesDest: List[Edge]
 )
@@ -26,44 +26,67 @@ class BellmanFSpec extends FunSuite {
   val inf = Double.PositiveInfinity
   val graph: SortedMap[String, Vertex] = SortedMap(
     "A" -> Vertex(
+      "A",
       0,
       List(Edge("A", "B", -1), Edge("A", "C", 4)) // from this vertex, I can go (through these edges) to these vertexes
     ),
     "B" -> Vertex(
+      "B",
       inf,
       List(Edge("B", "C", 3),Edge("B", "D", 2), Edge("B", "E", 2))
     ),
     "C" -> Vertex(
+      "C",
       inf,
       List()
     ),
     "D" -> Vertex(
+      "D",
       inf,
       List(Edge("D", "B", 1),Edge("D", "C", 5))
     ),
     "E" -> Vertex(
+      "E",
       inf,
       List(Edge("E", "D", -3))
     )
   )
 
 
-  def initialize_single_source(graph: SortedMap[String, Vertex], start: String) = {
+  def bellmanford(graph: SortedMap[String, Vertex]) = {
+    // relax
     for((keyVertex, fromVertex) <- graph) {
       for(edge <- fromVertex.edgesDest) {
         var toVertex = graph.get(edge.to).get
 
-        if(fromVertex.distance != inf || fromVertex.distance + edge.weight < toVertex.distance) {
+        if(fromVertex.distance != inf && fromVertex.distance + edge.weight < toVertex.distance) {
           toVertex.distance = fromVertex.distance + edge.weight
         }
 
-
       }
     }
+
+
+    //TODO check negative cycles
   }
 
 
   test("I can modify object inside of inmutable List") {
-    initialize_single_source(graph, "A")
+    bellmanford(graph)
+
+    // summarize result
+    var vertextWithWeights = SortedMap[String, Double]()
+    for((keyVertex, fromVertex) <- graph) {
+      vertextWithWeights = vertextWithWeights + (keyVertex -> fromVertex.distance)
+    }
+
+    assert(vertextWithWeights === Map(
+      "A" -> 0.0,
+      "B" -> -1.0,
+      "C" -> 2.0,
+      "D" -> -2.0,
+      "E" -> 1.0
+      )
+    )
   }
 }
