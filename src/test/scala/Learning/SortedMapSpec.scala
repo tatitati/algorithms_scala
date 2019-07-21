7b28875b8d7dcb00799c04f2acf60b573c686559
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-07-22 00:16:11 +0100

    more

--- src/test/scala/Learning/SortedMapSpec.scala
+++ src/test/scala/Learning/SortedMapSpec.scala
@@ -1,7 +1,9 @@
 package Learning
 
 import org.scalatest.FunSuite
+
 import scala.collection.SortedMap
+import scala.collection.mutable.ListMap
 
 class SortedMapSpec extends FunSuite {
 
@@ -16,6 +18,25 @@ class SortedMapSpec extends FunSuite {
     assert(map1 == SortedMap(
       "a" -> 5,
       "c" -> 5,
+      "x" -> 5,
+      "z" -> 5
+    ))
+  }
+
+  test("by default is mutable") {
+    var map1 = SortedMap(
+      "c" -> 5,
+      "z" -> 5,
+      "a" -> 5,
+      "x" -> 5
+    )
+
+    map1 += ("somethingnew" -> 4)
+
+    assert(map1 == SortedMap(
+      "a" -> 5,
+      "c" -> 5,
+      "somethingnew" -> 4,
       "x" -> 5,
       "z" -> 5
     ))
