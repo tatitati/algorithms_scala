3cab80c15747049a5d6b62ca005e2229d91ddc2c
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-07-22 00:00:17 +0100

    more

--- src/test/scala/Learning/ListMapSpec.scala
+++ src/test/scala/Learning/ListMapSpec.scala
@@ -1,10 +1,24 @@
 package Learning
 
 import org.scalatest.FunSuite
+import scala.collection.mutable.ListMap
 
 class ListMapSpec extends FunSuite {
 
-  test("ListMap") {
+  test("ListMap ") {
+    var map1 = ListMap(
+      "c" -> 5,
+      "z" -> 5,
+      "a" -> 5,
+      "x" -> 5
+    )
 
+
+    assert(map1 == ListMap(
+      "c" -> 5,
+      "a" -> 5,
+      "z" -> 5,
+      "x" -> 5
+    ), "when comparing, doesnt matter the orther")
   }
 }
