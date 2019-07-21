84faec691c56c75274eca93724279ad96fff665f
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-07-21 23:43:40 +0100

    more

--- src/test/scala/graph/shortestpath/FloydWarshall/FloydWarshallSpec.scala
+++ src/test/scala/graph/shortestpath/FloydWarshall/FloydWarshallSpec.scala
@@ -2,19 +2,19 @@ package graph.shortestpath.FloydWarshall
 
 import org.scalatest.FunSuite
 import scala.collection.mutable.Map
-import scala.collection.immutable.ListMap
+import scala.collection.mutable.ListMap
 
 class FloydWarshallSpec extends FunSuite {
 
-  def floydWarshall(G: ListMap[String, ListMap[String, Int]]): Unit = {
+  def floydWarshall(G: ListMap[String, ListMap[String, Int]]): (ListMap[String, ListMap[String, Double]], ListMap[String, ListMap[String, String]]) = {
       val infinity = Double.PositiveInfinity
-      var dist: Map[String, Map[String, Double]] = Map()
-      var pred: Map[String, Map[String, String]] = Map()
+      var dist: ListMap[String, ListMap[String, Double]] = ListMap()
+      var pred: ListMap[String, ListMap[String, String]] = ListMap()
 
       // init
       for((u, adjs) <- G) {
-        var distsU: Map[String, Double] = Map()
-        var predU: Map[String, String] = Map()
+        var distsU: ListMap[String, Double] = ListMap()
+        var predU: ListMap[String, String] = ListMap()
 
         for((v, _) <- G) {
           if (u == v) {
@@ -30,7 +30,6 @@ class FloydWarshallSpec extends FunSuite {
         dist += (u -> distsU)
 
         // set initial/original distances
-        println(dist)
         for((v, w) <- adjs) {
           dist(u).update(v, w)
           pred(u).update(v, u)
@@ -51,6 +50,7 @@ class FloydWarshallSpec extends FunSuite {
         }
       }
 
+    (dist, pred)
 
   }
 
@@ -62,9 +62,23 @@ class FloydWarshallSpec extends FunSuite {
         "4" -> ListMap("2" -> -1)
     )
 
-    floydWarshall(graph)
+    var (dist, pred) = floydWarshall(graph)
+
+    assert(dist == ListMap(
+      "1" -> ListMap("2" -> -1.0, "1" -> 0.0, "4" -> 0.0, "3" -> -2.0),
+      "2" -> ListMap("2" -> 0.0,  "1" -> 4.0, "4" -> 4.0, "3" -> 2.0),
+      "3" -> ListMap("2" -> 1.0,  "1" -> 5.0, "4" -> 2.0, "3" -> 0.0),
+      "4" -> ListMap("2" -> -1.0, "1" -> 3.0, "4" -> 0.0, "3" -> 1.0)
+    )
+    )
 
 
+    assert(pred == ListMap(
+      "1" -> ListMap("2" -> "4", "1" -> "",  "4" -> "3", "3" -> "1"),
+      "2" -> ListMap("2" -> "",  "1" -> "2", "4" -> "3", "3" -> "1"),
+      "3" -> ListMap("2" -> "4", "1" -> "2", "4" -> "3", "3" -> ""),
+      "4" -> ListMap("2" -> "4", "1" -> "2", "4" -> "",  "3" -> "1")
+    ))
   }
 
 }
