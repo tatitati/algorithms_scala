f4e8f96abe1d70b4a8cfbf3558f9e461418b3ab4
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-07-22 00:01:09 +0100

    renamed file

--- src/test/scala/Learning/SortedMapSpec.scala
+++ src/test/scala/Learning/SortedMapSpec.scala
@@ -6,12 +6,12 @@ import scala.collection.SortedMap
 class SortedMapSpec extends FunSuite {
 
   test("SortedMap sort by key the map") {
-      var map1 = SortedMap(
-        "c" -> 5,
-        "z" -> 5,
-        "a" -> 5,
-        "x" -> 5
-      )
+    var map1 = SortedMap(
+      "c" -> 5,
+      "z" -> 5,
+      "a" -> 5,
+      "x" -> 5
+    )
 
     assert(map1 == SortedMap(
       "a" -> 5,
