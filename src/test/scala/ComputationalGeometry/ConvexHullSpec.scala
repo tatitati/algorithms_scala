d4442e060dd6680120771f52114977a09f24385b
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-07-23 23:11:35 +0100

    added some P

--- src/test/scala/ComputationalGeometry/ConvexHullSpec.scala
+++ src/test/scala/ComputationalGeometry/ConvexHullSpec.scala
@@ -3,5 +3,27 @@ package ComputationalGeometry
 import org.scalatest.FunSuite
 
 class ConvexHullSpec extends FunSuite {
-    
+
+    def convexhull(P: List[List[Int]]): Unit = {
+
+    }
+
+    test("Convex-hull") {
+
+      // to-do the sorting should happen into the algorithm as well...
+      val P = List(
+        List(1, 2),
+        List(2, 1),
+        List(2, 2),
+        List(2, 3),
+        List(3, 3),
+        List(3, 4),
+        List(4, 1),
+        List(4, 3),
+        List(4, 5),
+        List(5, 2)
+      )
+
+      convexhull(P)
+    }
 }
