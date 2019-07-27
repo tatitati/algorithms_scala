be14afa1621f2047440c82f5635144d9d7ccda66
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-07-27 14:40:38 +0100

    jesus

--- src/test/scala/ComputationalGeometry/CrossProductSpec.scala
+++ src/test/scala/ComputationalGeometry/CrossProductSpec.scala
@@ -8,12 +8,6 @@ class CrossProductSpec extends FunSuite {
     p1(0) * p2(1) - p1(1) * p2(0)
   }
 
-  def crossproductTwoSegments(p1: List[Int], p2: List[Int], p3: List[Int]): Int = {
-    val p1p2 = List(p2(0) - p1(0), p2(1) - p1(1))
-    val p2p3 = List(p3(0) - p2(0), p3(1) - p2(1))
-    crossproductTwoPoints(p1p2, p2p3)
-  }
-
   test("cross product of two points") {
     val p1 = List(2, 4)
     val p2 = List(4, 1)
@@ -28,20 +22,25 @@ class CrossProductSpec extends FunSuite {
     assert(crossproductTwoPoints(p2, p1) > 0)
   }
 
-
-  test("cross product of two segments turning left in p2") {
+  test("cross product of two segments turning LEFT in p2") {
     val p1 = List(2, 1)
     val p2 = List(5, 3)
     val p3 = List(1, 4)
 
-    assert(crossproductTwoSegments(p1, p2, p3) > 0)
+    val p2_ = List(p2(0) - p1(0), p2(1) - p1(1))
+    val p3_ = List(p3(0) - p1(0), p3(1) - p1(1))
+
+    assert(crossproductTwoPoints(p3_, p2_) < 0) // order matter. From p2 to p3 I have to turn let (p3 x p2 < 0)
   }
 
-  test("cross product of two segments turning right on p2") {
+  test("cross product of two segments turning RIGHT on p2") {
     val p1 = List(2, 1)
     val p2 = List(4, 3)
     val p3 = List(7, 2)
 
-    assert(crossproductTwoSegments(p1, p2, p3) < 0)
+    val p2_ = List(p2(0) - p1(0), p2(1) - p1(1))
+    val p3_ = List(p3(0) - p1(0), p3(1) - p1(1))
+
+    assert(crossproductTwoPoints(p3_, p2_) > 0) // from p2 to p3 I have to turn right ( p3 x p2 > 0)
   }
 }
