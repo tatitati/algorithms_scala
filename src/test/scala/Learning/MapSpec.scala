064a17236617d1a1cefbed760a5bb7231a341e1e
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-11-03 16:02:43 +0000

    added some more checkes

--- src/test/scala/Learning/MapSpec.scala
+++ src/test/scala/Learning/MapSpec.scala
@@ -4,7 +4,42 @@ import org.scalatest.FunSuite
 
 class MapSpec extends FunSuite {
 
-  test("Map") {
+  test("Can remove/add items from map") {
+    val a = Map(
+      "a" -> List("AA1", "AA2", "AA3"),
+      "b" -> List("BB1", "BB2", "BB3"),
+      "c" -> List("CC1", "CC2", "CC3")
+    )
 
+    // remove
+    assert(Map(
+        "a" -> List("AA1", "AA2", "AA3"),
+        "c" -> List("CC1", "CC2", "CC3")
+    ) == a - "b")
+
+    // add
+    assert(Map(
+      "a" -> List("AA1", "AA2", "AA3"),
+      "b" -> List("BB1", "BB2", "BB3"),
+      "c" -> List("CC1", "CC2", "CC3"),
+      "d" -> List("DD1", "DD2", "DD3")
+    ) == a ++ Map("d" -> List("DD1", "DD2", "DD3")))
+  }
+
+  test("filter keys") {
+    val a = Map(
+      "A"    -> "sumi",
+      "B"     -> "2",
+      "C" -> "0",
+      "D"  -> "1",
+      "E"    -> "govind singh"
+    )
+
+    val b = a.filterKeys(_ != "A")
+    assert(Map(
+      "E" -> "govind singh",
+      "B" -> "2",
+      "C" -> "0",
+      "D" -> "1") == b)
   }
 }
