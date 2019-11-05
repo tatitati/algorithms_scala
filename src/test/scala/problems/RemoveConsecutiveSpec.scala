2721f681941a95ffc98c317589240a8e7af0079f
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-11-05 00:24:02 +0000

    added some interesting loops..

--- src/test/scala/problems/RemoveConsecutiveSpec.scala
+++ src/test/scala/problems/RemoveConsecutiveSpec.scala
@@ -1,14 +1,45 @@
 package problems
 
 import org.scalatest.FunSuite
+import scala.annotation.tailrec
 
 class RemoveConsecutiveSpec extends FunSuite {
 
-  def compress(lst: List[Any]): List[Any] = {
-
+  // standard recursive
+  def compressRecursive(ls: List[Any]): List[Any] = ls match {
+    case Nil => Nil
+    case v :: tail => v :: compressRecursive(tail.dropWhile(_ == v))
   }
 
+  // tail recursive
+  def compressTailRecursive(ls: List[Any]): List[Any] = {
+    @tailrec
+    def run(ls: List[Any], result: List[Any]): List[Any] = {
+      ls match {
+        case Nil => result
+        case v :: tail => run(tail.dropWhile(_ == v), result ++ List(v))
+      }
+    }
+
+    run(ls, List())
+  }
+
+  // functional
+  def compress3(ls: List[Any]): List[Any] = ???
+
   test("Eliminate consecutive duplicates of list elementss") {
-    assert(List('a, 'b, 'c, 'a, 'd, 'e) == compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)))
+
+    assert(List('a, 'b, 'c, 'a, 'd, 'e) == compressRecursive(    List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)))
+    assert(List('a, 'b, 'c, 'a, 'd, 'e) == compressTailRecursive(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)))
+  }
+
+  test("dropWhile") {
+    val a = List("b", "b", "b", "b", "c", "c")
+
+    val result1 = a.dropWhile(_ == "b")
+    val result2 = a.dropWhile(_ == "c")
+
+    assert(List("c", "c") == result1)
+    assert(List("b", "b", "b", "b", "c", "c") == result2)
   }
 }
