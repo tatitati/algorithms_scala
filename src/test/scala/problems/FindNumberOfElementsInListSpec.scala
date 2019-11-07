8ba39e59a5ce435cb36dee412b7d54c57f3cd46b
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-11-07 11:27:07 +0000

    more

---
+++ src/test/scala/problems/FindNumberOfElementsInListSpec.scala
@@ -0,0 +1,33 @@
+package problems
+
+import org.scalatest.FunSuite
+
+import scala.annotation.tailrec
+
+class FindNumberOfElementsInListSpec extends FunSuite {
+
+    def recursive(lst: List[Any]) : Int = {
+        lst match {
+          case Nil => 0
+          case _ :: tail => 1 + recursive(tail)
+        }
+    }
+
+    def tailRecursive(lst: List[Any]) : Int = {
+      @tailrec
+      def repeat(l: List[Any], counter: Int): Int = {
+        l match {
+          case Nil => counter
+          case _ :: tail => repeat(tail, counter + 1)
+        }
+      }
+
+      repeat(lst, 0)
+    }
+
+    test("test") {
+      val mylist = List(2, 4, 6, 33, 55, 66, 22)
+      assert(7 == recursive(mylist))
+      assert(7 == tailRecursive(mylist))
+    }
+}
