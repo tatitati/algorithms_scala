472f8db82572c14f19f3e77d93dcc5451c2fe95d
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-11-07 15:11:31 +0000

    more

--- src/test/scala/problems/DropEVeryNELementSpec.scala
+++ src/test/scala/problems/DropEVeryNELementSpec.scala
@@ -2,8 +2,10 @@ package problems
 
 import org.scalatest.FunSuite
 
+import scala.annotation.tailrec
+
 class DropEVeryNELementSpec extends FunSuite {
-  
+
   def recursive[A](n: Int, l: List[A]): List[A] = {
     def repeat[A](i: Int, l: List[A]): List[A] = {
       l match {
@@ -16,8 +18,22 @@ class DropEVeryNELementSpec extends FunSuite {
     repeat(n, l)
   }
 
-  test("testdef repeat[A](n: Int, l: List[A]): List[A] = {") {
+  def tailRecursive[A](n: Int, l: List[A]): List[A] = {
+    @tailrec
+    def repeat(i: Int, l: List[A], acc: List[A]): List[A] = {
+      l match {
+        case Nil => acc
+        case h :: t if i == 0 => repeat(n, t, acc)
+        case h :: t if i > 0 => repeat(i-1, t, acc :+ h)
+      }
+    }
+
+    repeat(n, l, List())
+  }
+
+  test("test") {
     val mylist = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
     assert(List('a, 'b, 'c, 'e, 'f, 'g, 'i, 'j, 'k) == recursive(3, mylist))
+    assert(List('a, 'b, 'c, 'e, 'f, 'g, 'i, 'j, 'k) == tailRecursive(3, mylist))
   }
 }
