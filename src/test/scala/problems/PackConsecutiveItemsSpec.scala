f8a0c4fe8c095611bb5a53a61f7cecd992753f4d
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-11-07 13:57:50 +0000

    more

--- src/test/scala/problems/PackConsecutiveItemsSpec.scala
+++ src/test/scala/problems/PackConsecutiveItemsSpec.scala
@@ -4,7 +4,17 @@ import org.scalatest.FunSuite
 
 class PackConsecutiveItemsSpec extends FunSuite {
 
-  test("test") {
-
+  def recursive[A](l: List[A]): List[List[A]] = {
+    if(l.isEmpty) List()
+    else {
+      val (packed, unpacked) = l span (_ == l.head)
+      packed :: recursive(unpacked)
+    }
   }
+
+  test("test") {
+    val mylist = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+    assert(List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e)) == recursive(mylist))
+  }
+
 }
