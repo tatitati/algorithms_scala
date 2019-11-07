f9ca5a0b002ec5de05567bc707ac9a23f8fe48dd
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-11-07 12:35:52 +0000

    more

--- src/test/scala/problems/FlattenlistSpec.scala
+++ src/test/scala/problems/FlattenlistSpec.scala
@@ -4,12 +4,14 @@ import org.scalatest.FunSuite
 
 class FlattenlistSpec extends FunSuite {
 
-  private def flatten(listOfLists: List[Any]): List[Any] = listOfLists flatMap {
-    case a: List[_] => flatten(a)
-    case x => List(x)
+  private def recursive(listOfLists: List[Any]): List[Any] = {
+    listOfLists flatMap {
+      case a: List[_] => recursive(a)
+      case x => List(x)
+    }
   }
 
   test(" Flatten a nested list structure.") {
-    assert(List(1, 1, 2, 3, 5, 8) == flatten(List(List(1, 1), 2, List(3, List(5, 8)))))
+    assert(List(1, 1, 2, 3, 5, 8) == recursive(List(List(1, 1), 2, List(3, List(5, 8)))))
   }
 }
