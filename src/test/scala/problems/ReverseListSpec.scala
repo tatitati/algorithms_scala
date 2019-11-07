9f1e13381fd028ef23880b574a086928ddc659df
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-11-07 12:13:27 +0000

    more

--- src/test/scala/problems/ReverseListSpec.scala
+++ src/test/scala/problems/ReverseListSpec.scala
@@ -2,6 +2,8 @@ package problems
 
 import org.scalatest.FunSuite
 
+import scala.annotation.tailrec
+
 class ReverseListSpec extends FunSuite {
 
   def recursive[A](l: List[A]): List[A] = {
@@ -11,8 +13,21 @@ class ReverseListSpec extends FunSuite {
     }
   }
 
+  def tailRecursive[A](list: List[A]): List[A] = {
+    @tailrec
+    def repeat[A](l: List[A], acc: List[A]): List[A] = {
+      l match {
+        case Nil => acc
+        case head :: tail => repeat(tail, head +: acc )
+      }
+    }
+
+    repeat(list, List())
+  }
+
   test("test") {
     val mylist = List(2, 4, 6, 33, 55, 66, 22)
     assert(List(22, 66, 55, 33, 6, 4, 2) == recursive(mylist))
+    assert(List(22, 66, 55, 33, 6, 4, 2) == tailRecursive(mylist))
   }
 }
