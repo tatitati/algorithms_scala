47ae475ad2c1b66531dec9e1d5832e9e5a21ab55
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-11-07 10:37:51 +0000

    some more problems

---
+++ src/test/scala/problems/FindLastElementSpec.scala
@@ -0,0 +1,27 @@
+package problems
+
+import org.scalatest.FunSuite
+
+// P01 (*) Find the last element of a list.
+//     Example:
+//     scala> last(List(1, 1, 2, 3, 5, 8))
+//     res0: Int =
+
+class FindLastElementSpec extends FunSuite {
+    val mylist = List(2, 4, 6, 33, 55, 66, 22)
+
+    test("last builtin") {
+      assert(22 == builtinLast(mylist))
+      assert(22 == recursiveLast(mylist))
+    }
+
+    def builtinLast[A](l: List[A]): A = l.last
+
+    def recursiveLast[A](l: List[A]): A = {
+      l match {
+        case last :: Nil => last
+        case _ :: tail => recursiveLast(tail)
+        case Nil =>  throw new NoSuchElementException
+      }
+    }
+}
