c971c30a7762ede9384301fcbcfb6feea63cc449
Francisco Albusac <francisco.albusac@bbc.co.uk>
2019-11-07 11:55:25 +0000

    more

--- src/test/scala/problems/ReverseListSpec.scala
+++ src/test/scala/problems/ReverseListSpec.scala
@@ -4,18 +4,15 @@ import org.scalatest.FunSuite
 
 class ReverseListSpec extends FunSuite {
 
-  //def recursive[A](l: List[A]): List[A] = {
-  //  l match {
-  //    case Nil => Nil
-  //    case head :: tail =>
-  //  }
-  //}
+  def recursive[A](l: List[A]): List[A] = {
+    l match {
+      case Nil => Nil
+      case head :: tail =>recursive(tail) ::: List(head)
+    }
+  }
 
-  test("::: vs ::") {
-    val a = List(2, 4, 6, 33, 55, 66, 22)
-    val b = List(2, 5, 6, 7, 8, 9)
-
-    assert(List(2, 4, 6, 33, 55, 66, 22, 2, 5, 6, 7, 8, 9) == a ::: b)
-    assert(List(List(2, 4, 6, 33, 55, 66, 22), 2, 5, 6, 7, 8, 9) == a :: b)
+  test("test") {
+    val mylist = List(2, 4, 6, 33, 55, 66, 22)
+    assert(List(22, 66, 55, 33, 6, 4, 2) == recursive(mylist))
   }
 }
